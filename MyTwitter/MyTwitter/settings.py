bcedd739577dffdefaf40416111da927a7ae9032
yk-jha <yjha5965@gmail.com>
2025-06-24 14:37:45 +0530

    sett

--- MyTwitter/MyTwitter/settings.py
+++ MyTwitter/MyTwitter/settings.py
@@ -57,7 +57,9 @@ WSGI_APPLICATION = 'MyTwitter.wsgi.application'
 # Database Configuration for PostgreSQL on Render
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
